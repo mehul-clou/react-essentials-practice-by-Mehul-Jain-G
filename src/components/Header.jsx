ce54fb6fb650182346a84b318315c6b85f2ae3fb
mj <85698909+mehul-clou@users.noreply.github.com>
2023-12-09 13:53:42 +0000

    Update 3 files
    that the originally

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+export default function Header() {
+  return ( 
+    <header>
+      <img src={logo} alt="A Calculator" />
+      <h1>Investment Calculator</h1>
+      <h2>hi close sandebox fist</h2>
+    </header>
+  );
+}
